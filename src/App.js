022ffdca2c1bf9de865a01cdc0a7370b6292e25c
elmik <elmik.karl@gmail.com>
2024-04-10 14:34:47 +0300

    Made invoices section

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Dashboard from "./pages/dashboard"
 import Topbar from "./pages/global/Topbar";
 import Sidebar from "./pages/global/Sidebar";
 import Team from "./pages/team"
-// import Invoices from "./pages/invoices"
+import Invoices from "./pages/invoices"
 import Contacts from "./pages/contacts"
 // import Bar from "./pages/bar"
 // import Form from "./pages/form"
@@ -31,7 +31,7 @@ function App() {
                   <Route path="/" element={<Dashboard />}/>
                   {<Route path="/team" element={<Team />}/>}
                   {<Route path="/contacts" element={<Contacts />}/>}
-                  {/* <Route path="/invoices" element={<Invoices />}/> */}
+                  {<Route path="/invoices" element={<Invoices />}/>}
                   {/* <Route path="/form" element={<Form />}/> */}
                   {/* <Route path="/bar" element={<Bar />}/> */}
                   {/* <Route path="/line" element={<Line />}/> */}
---
+++ src/pages/invoices/index.jsx
@@ -0,0 +1,91 @@
+import { useTheme, Box } from "@mui/material"
+import { tokens } from "../../theme"
+import Header from "../../components/Header"
+import { DataGrid } from "@mui/x-data-grid"
+import { mockDataInvoices } from "../../data/mockData"
+
+
+const Invoices = () => {
+    const theme = useTheme()
+    const colors = tokens(theme.palette.mode)
+
+    const columns = [
+        {
+            field: "id",
+            headerName: "ID",
+            headerClassName: "admin-app--header",
+            flex: 0.5
+        },
+        {
+            field: "name",
+            headerName: "Name",
+            headerClassName: "admin-app--header",
+            cellClassName: "name-column--cell",
+            flex: 1
+        },
+        {
+            field: "phone",
+            headerClassName: "admin-app--header",
+            headerName: "Phone Number",
+            flex: 1
+        },
+        {
+            field: "email",
+            headerClassName: "admin-app--header",
+            headerName: "Email",
+            flex: 1
+        },
+        {
+            field: "cost",
+            headerName: "Cost",
+            headerClassName: "admin-app--header",
+            flex: 1,
+            cellClassName: "cost-column--cell"
+        },
+        {
+            field: "date",
+            headerName: "Date",
+            headerClassName: "admin-app--header",
+            flex: 1
+        },
+    ]
+
+    return(
+        <Box m="20px">
+            <Header title="INVOICES" subtitle="Here are all the invoices"></Header>
+            <Box
+                m="40px 0 0 0"
+                height= "75vh"
+                sx={{
+                    "& .MuiDataGrid-root": {
+                        border: "none"
+                    },
+                    "& .MuiDataGrid-cell": {
+                        border: "none"
+                    },
+                    "& .admin-app--header": {
+                        backgroundColor: colors.blueAccent[700],
+                        borderBottom: "10px"
+                    },
+                    "& .cost-column--cell": {
+                        color: colors.greenAccent[300]
+                    },
+                    "& .MuiDataGrid-virtualScroller": {
+                        backgroundColor: colors.primary[400],
+                    },
+                    "& .MuiDataGrid-footerContainer": {
+                        borderTop: "none",
+                        backgroundColor: colors.blueAccent[700],
+                    },
+                    "& .MuiDataGrid-virtualScrollerContent": {
+                        border: "none",
+                    }
+                }}
+            >
+                <DataGrid rows={mockDataInvoices} columns={columns}/>
+            </Box>
+        </Box>
+    )
+}
+
+export default Invoices
