fc241f9dfbae4c9398949b6436f8f1b36147b558
elmik <elmik.karl@gmail.com>
2024-04-15 22:20:13 +0300

    Added imports and routes to App.js

--- src/App.js
+++ src/App.js
@@ -7,11 +7,11 @@ import Sidebar from "./pages/global/Sidebar";
 import Team from "./pages/team"
 import Invoices from "./pages/invoices"
 import Contacts from "./pages/contacts"
-// import Bar from "./pages/bar"
+import Bar from "./pages/bar"
 import Form from "./pages/form"
 // import Line from "./pages/line"
 // import Pie from "./pages/pie"
-// import FAQ from "./pages/faq"
+import FAQ from "./pages/faq"
 import Calendar from "./pages/calendar"
 // import Geography from "./pages/geography"
 
@@ -33,10 +33,10 @@ function App() {
                   {<Route path="/contacts" element={<Contacts />}/>}
                   {<Route path="/invoices" element={<Invoices />}/>}
                   {<Route path="/form" element={<Form/>}/>}
-                  {/* <Route path="/bar" element={<Bar />}/> */}
+                  {<Route path="/bar" element={<Bar />}/>}
                   {/* <Route path="/line" element={<Line />}/> */}
                   {/* <Route path="/pie" element={<Pie />}/> */}
-                  {/* <Route path="/faq" element={<FAQ />}/> */}
+                  {<Route path="/faq" element={<FAQ />}/>}
                   {<Route path="/calendar" element={<Calendar />}/>}
                   {/* <Route path="/geography" element={<Geography />}/> */}
                 </Routes>
